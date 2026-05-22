5eae73398609557b23af68b8e47540981a0eb842
bazikin-ux <bassotegameplays@gmail.com>
2026-05-22 16:47:46 -0300

    Usar init point no checkout Mercado Pago

--- app/page.tsx
+++ app/page.tsx
@@ -153,10 +153,10 @@ export default function Home() {
       return;
     }
 
-    if (data.sandbox_init_point) {
-      window.location.href = data.sandbox_init_point;
-    } else if (data.init_point) {
+    if (data.init_point) {
       window.location.href = data.init_point;
+    } else if (data.sandbox_init_point) {
+      window.location.href = data.sandbox_init_point;
     } else {
       alert("Pagamento criado, mas o link do checkout não foi retornado.");
     }
